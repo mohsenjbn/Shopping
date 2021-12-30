1f369044cedf9ea092c201386116982cf82ee5bf
mohsenjbn <91272908+mohsenjbn@users.noreply.github.com>
2021-12-30 18:41:36 +0330

    Product Category Implemented

---
+++ Shop/01-framework/Application/ValidationMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace _01_framework.Application
+{
+    public class ValidationMessages
+    {
+        public const string IsRequired = "این مقدار نمی تواند خالی باشد.";
+    }
+}
--- Shop/Sh.Application.Constract/ProductCategory/CreateProductCategory.cs
+++ Shop/Sh.Application.Constract/ProductCategory/CreateProductCategory.cs
@@ -1,20 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using _01_framework.Application;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShopiManagement.Application.Contracts.ProductCategory
 {
     public class CreateProductCategory
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Picture { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Describtion { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string KeyWords { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string MetaDescribtion { get;  set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Slug { get;  set; }
     }
 }
